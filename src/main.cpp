cce579aac04a6db30e62555be18d3cea2b45911d
SobczykMichal <sobczykmichal01@gmail.com>
2025-03-19 15:25:32 +0100

    swiatla,dht11,deszczu,ruchu,ognia, wysylanie z znacznikiem czasowym

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
   #include <ESP8266WiFi.h>
 #endif
 #include <Firebase_ESP_Client.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 #include "DHT.h"
@@ -27,8 +29,13 @@ FirebaseConfig config;
 #define RAIN_SENSOR_PIN 36   // analogowy
 #define FLAME_SENSOR_PIN 39  // analogowy
 #define PIR_SENSOR_PIN 25    // cyfrowy
+#define LIGHT_SENSOR_PIN 34  // analogowy czujnik natężenia światła
 
 DHT dht(DHTPIN, DHTTYPE);
+
+// NTP
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 3600, 60000); // UTC+1 dla Polski (CET, zima)
 
 unsigned long sendDataPrevMillis = 0;
 bool signupOK = false;
@@ -49,6 +56,8 @@ void setup() {
   Serial.println(WiFi.localIP());
   Serial.println();
 
+  timeClient.begin();
+
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
 
@@ -68,42 +77,81 @@ void loop() {
   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)) {
     sendDataPrevMillis = millis();
 
+    timeClient.update();
+
     // Odczyt danych z czujników
     float temperature = dht.readTemperature();
     float humidity = dht.readHumidity();
     int rainValue = analogRead(RAIN_SENSOR_PIN);
     int flameValue = analogRead(FLAME_SENSOR_PIN);
     bool motionDetected = digitalRead(PIR_SENSOR_PIN);
+    int lightIntensity = analogRead(LIGHT_SENSOR_PIN);
 
-    // Wysyłanie danych na Firebase
+    // Pobierz timestamp i sformatowany czas
+    String timestamp = String(timeClient.getEpochTime());
+    String formattedTime = timeClient.getFormattedTime();
+
+    // Wyświetl czas w Serial Monitor
+    Serial.print("Data sent at: ");
+    Serial.println(formattedTime);
+
+    String basePath = "history/" + timestamp;
+
+    // Wysyłanie danych historycznych na Firebase
+    if (Firebase.RTDB.setFloat(&fbdo, basePath + "/temperature", temperature)) {
+      Serial.println("Historical Temperature sent.");
+    } else {
+      Serial.println("Historical Temperature FAILED: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.setFloat(&fbdo, basePath + "/humidity", humidity)) {
+      Serial.println("Historical Humidity sent.");
+    } else {
+      Serial.println("Historical Humidity FAILED: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.setInt(&fbdo, basePath + "/rain_sensor", rainValue)) {
+      Serial.println("Historical Rain sensor sent.");
+    } else {
+      Serial.println("Historical Rain sensor FAILED: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.setInt(&fbdo, basePath + "/flame_sensor", flameValue)) {
+      Serial.println("Historical Flame sensor sent.");
+    } else {
+      Serial.println("Historical Flame sensor FAILED: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.setBool(&fbdo, basePath + "/motion_detected", motionDetected)) {
+      Serial.println("Historical Motion status sent.");
+    } else {
+      Serial.println("Historical Motion status FAILED: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.setInt(&fbdo, basePath + "/light_intensity", lightIntensity)) {
+      Serial.println("Historical Light intensity sent.");
+    } else {
+      Serial.println("Historical Light intensity FAILED: " + fbdo.errorReason());
+    }
+
+    // Opcjonalnie: Aktualne dane do "sensors"
     if (Firebase.RTDB.setFloat(&fbdo, "sensors/temperature", temperature)) {
-      Serial.println("Temperature sent.");
-    } else {
-      Serial.println("Temperature FAILED: " + fbdo.errorReason());
+      Serial.println("Live Temperature sent.");
     }
-
     if (Firebase.RTDB.setFloat(&fbdo, "sensors/humidity", humidity)) {
-      Serial.println("Humidity sent.");
-    } else {
-      Serial.println("Humidity FAILED: " + fbdo.errorReason());
+      Serial.println("Live Humidity sent.");
     }
-
     if (Firebase.RTDB.setInt(&fbdo, "sensors/rain_sensor", rainValue)) {
-      Serial.println("Rain sensor sent.");
-    } else {
-      Serial.println("Rain sensor FAILED: " + fbdo.errorReason());
+      Serial.println("Live Rain sensor sent.");
     }
-
     if (Firebase.RTDB.setInt(&fbdo, "sensors/flame_sensor", flameValue)) {
-      Serial.println("Flame sensor sent.");
-    } else {
-      Serial.println("Flame sensor FAILED: " + fbdo.errorReason());
+      Serial.println("Live Flame sensor sent.");
     }
-
     if (Firebase.RTDB.setBool(&fbdo, "sensors/motion_detected", motionDetected)) {
-      Serial.println("Motion status sent.");
-    } else {
-      Serial.println("Motion status FAILED: " + fbdo.errorReason());
+      Serial.println("Live Motion status sent.");
+    }
+    if (Firebase.RTDB.setInt(&fbdo, "sensors/light_intensity", lightIntensity)) {
+      Serial.println("Live Light intensity sent.");
     }
 
     Serial.println("All data sent to Firebase.\n");
